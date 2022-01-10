1b710c2aa635bea6665428c963be69f2743cfaee
rdbo <rdbodev@gmail.com>
2022-01-09 21:49:17 -0300

    fixed logger print

--- ronix/utils/logger.cpp
+++ ronix/utils/logger.cpp
@@ -1,4 +1,6 @@
 #include "logger.hpp"
+#include <sys/stat.h>
+#include <fcntl.h>
 
 Logger::Logger(std::string output_file)
 {
@@ -22,7 +24,15 @@ void Logger::Log(const char *fmt, ...)
 
 void Logger::Log(const char *fmt, va_list arg)
 {
-	std::fprintf(this->file, "[RONIX] ");
+	int fd = open(this->filepath.c_str(), O_RDONLY);
+	if (fd != -1) {
+		char c;
+		lseek(fd, -1, SEEK_END);
+		int result = read(fd, reinterpret_cast<void *>(&c), sizeof(c));
+		if (result == 0 || c == '\n')
+			std::fprintf(this->file, "[RONIX] ");
+		close(fd);
+	}
 	std::vfprintf(this->file, fmt, arg);
 	std::fflush(this->file);
 }
