c6f9330380e4753a4f374acebc0985af96567c70
rdbo <rdbodev@gmail.com>
2025-01-18 19:53:58 +0000

    fixed static build

--- compile_static.sh
+++ compile_static.sh
@@ -2,4 +2,4 @@
 
 mkdir -p build
 
-cc -o build/libronix.so -std=c++11 -m32 -shared -fPIC -I /usr/include/SDL2 -I . -I imgui -I hl2sdk $(find hl2sdk -type d -exec printf "%s" "-I {} " \;) -I ronix -I json -fpermissive $(find ronix -type f -name "*.cpp") $(find imgui -type f -name "*.cpp") -pthread
+cc -o build/libronix.so -std=gnu++11 -m32 -shared -fPIC -I /usr/include/SDL2 -I . -I imgui -I hl2sdk $(find hl2sdk -type d -exec printf "%s" "-I {} " \;) -I ronix -I json -fpermissive $(find ronix -type f -name "*.cpp") $(find imgui -type f -name "*.cpp") -pthread -Bstatic -lc -lgcc -lstdc++
